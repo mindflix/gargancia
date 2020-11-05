13ea9458e9483e753053231b71a6268c16a5b1e7
Nicolas Demol <nyco.demol@gmail.com>
2020-11-05 19:59:28 +0100

    yolo

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:random_words/random_words.dart';
+import './random_words.dart';
 
 void main() => runApp(MyApp());
 
@@ -9,39 +9,5 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
         theme: ThemeData(primaryColor: Colors.indigo[900]),
         home: RandomWords());
-  }
-}
-
-class RandomWords extends StatefulWidget {
-  @override
-  RandomWordsState createState() => RandomWordsState();
-}
-
-class RandomWordsState extends State<RandomWords> {
-  final _randomWordPair = <WordPair>[];
-
-  Widget _buildList() {
-    return ListView.builder(
-        padding: const EdgeInsets.all(1),
-        itemBuilder: (context, item) {
-          if (item.isOdd) return Divider();
-
-          final index = item ~/ 2;
-
-          if (index >= _randomWordPair.length) {
-            _randomWordPair.addAll(generateWordPairs().take(10));
-          }
-          return _buildRow(_randomWordPair[index]);
-        });
-  }
-
-  Widget _buildRow(WordPair pair) {
-    return ListTile(
-        title: Text(pair.asPascalCase, style: TextStyle(fontSize: 18)));
-  }
-
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(title: Text('Gargancia')), body: _buildList());
   }
 }
